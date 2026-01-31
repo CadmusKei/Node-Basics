38235b5840ca15d08ba3bbb44f7e643e6c69d542
Kei <maxwell.kei.farouk@gmail.com>
2026-01-31 21:11:34 +0200

    Fix response status code to 200 for successful requests

--- http-Module/server.js
+++ http-Module/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.socket.remoteAddress, "req");
-  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello World!");
 });
 
