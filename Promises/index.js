ba317c1bb99ffe0827c32f75ed956a4eaddf5870
Kei <maxwell.kei.farouk@gmail.com>
2026-02-04 12:13:04 +0200

    Fix typo in divide function name and update usage example

--- Promises/index.js
+++ Promises/index.js
@@ -2,7 +2,7 @@ function delayFn(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function deivideFn(a, b) {
+function divideFn(a, b) {
   return new Promise((resolve, reject) => {
     if (b === 0) {
       reject("Cannot divide from 0");
@@ -17,6 +17,18 @@ delayFn(2000).then(() => {
   console.log("Promise Lecture Ends");
 });
 
-deivideFn(20, 2)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+// deivideFn(20, 2)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+async function displayMath(a, b) {
+  try {
+    console.log("Dividing...");
+    if (b === 0) throw new Error("Cannot divide by 0");
+    return ("The answer is: ", a / b);
+  } catch (error) {
+    console.log("Could not divide! Error: ", error);
+  }
+}
+
+console.log(await displayMath(20, 4));
